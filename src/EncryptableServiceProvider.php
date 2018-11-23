69d9db47b92d2e307a0d3716dab4a0787831981b
Alexey <info@khachatryan.org>
2018-11-23 14:49:36 +0400

    Update EncryptableServiceProvider.php

--- src/EncryptableServiceProvider.php
+++ src/EncryptableServiceProvider.php
@@ -1,31 +1,50 @@
 <?php
 
-
-
 namespace Alkhachatryan\Encryptable;
 
 use Illuminate\Support\ServiceProvider;
-use Alkhachatryan\Encryptable\Encryptable;
 
 class EncryptableServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
     }
 
     /**
-     * Register the application services.
+     * Register any package services.
      *
      * @return void
      */
     public function register()
     {
         $this->app->alias(Encryptable::class, 'encryptable');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['encryptable'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole()
+    {
     }
 }
