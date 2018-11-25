fcad1bfa6bf3496fe873c78d259fd5570ef9ca03
alexey <alexey.khachatryan@gmail.com>
2018-11-25 16:12:38 +0400

    Checking for NULL in the set and get methods. In case if value is NULL - return NULL

--- src/Encryptable.php
+++ src/Encryptable.php
@@ -1,22 +1,23 @@
 <?php
 
-
 namespace Alkhachatryan\Encryptable;
 use Illuminate\Support\Facades\Crypt;
 
 trait Encryptable
 {
     /**
-     * Decrypt data from DB
+     * Decrypt data from DB table.
+     * Decrypt only in that case if the value is not NULL.
+     * Otherwise attribute will be NULL.
      *
      * @param mixed $key
      * @return mixed $value
-    */
+     */
     public function getAttribute($key)
     {
         $value = parent::getAttribute($key);
 
-        if (in_array($key, $this->encryptable)) {
+        if (in_array($key, $this->encryptable) && null !== $value) {
             $value = Crypt::decrypt($value);
         }
 
@@ -24,7 +25,9 @@ trait Encryptable
     }
 
     /**
-     * Encrypt data to DB
+     * Encrypt incoming data to DB table.
+     * Encrypt only in that case if the value is not NULL.
+     * Otherwise attribute will be NULL.
      *
      * @param mixed $key
      * @param mixed $value
@@ -32,7 +35,7 @@ trait Encryptable
      */
     public function setAttribute($key, $value)
     {
-        if (in_array($key, $this->encryptable)) {
+        if (in_array($key, $this->encryptable) && null !== $value) {
             $value = Crypt::encrypt($value);
         }
 
